ef7967b49817b5e534d36d231b5860a24852ad67
Julio Cesar <jcmljunior@gmail.com>
2020-07-05 19:24:42 -0300

    importado modulos typescript

---
+++ src/@types/index.d.ts
@@ -0,0 +1,23 @@
+import { AddonConfig } from './core/addon';
+import { AddonsManagerConfig } from './core/addons-manager';
+import { AssistantManagerConfig } from './core/assistant-manager';
+import { BootletConfig } from './core/bootlet';
+import { BootletsManagerConfig } from './core/bootlets-manager';
+import { ConfigManagerConfig } from './core/config-manager';
+import { EvebyConfig } from './core/eveby';
+import { GuardManagerConfig } from './core/guard-manager';
+import { SentinelaManagerConfig } from './core/sentinela-manager';
+import { StorageManagerConfig } from './core/storage-manager';
+
+export {
+  AddonConfig,
+  AddonsManagerConfig,
+  AssistantManagerConfig,
+  BootletConfig,
+  BootletsManagerConfig,
+  ConfigManagerConfig,
+  EvebyConfig,
+  GuardManagerConfig,
+  SentinelaManagerConfig,
+  StorageManagerConfig,
+};
