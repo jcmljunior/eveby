a1ec93ef92eb33da8b78c69f75ab7ef6663fce77
Julio Cesar <jcmljunior@gmail.com>
2020-07-05 20:30:44 -0300

    adicionado configurações do projeto

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+export = {
+  debug: true,
+  node_env: 'dev',
+  database: {},
+};
--- src/core/config-manager.ts
+++ src/core/config-manager.ts
@@ -1,4 +1,59 @@
+import { Collection } from 'discord.js';
+import _config from '../config';
+
 /**
  * Responsavel pela administração das configurações do projeto.
  */
-export default class ConfigManager {}
+export default class ConfigManager {
+  private config: Collection<any, any>;
+
+  constructor() {
+    this.config = new Collection();
+    this.register(_config);
+  }
+
+  register(options: any): any {
+    for (const key in options) {
+      if (typeof options[key] === 'object') {
+        /**
+         * Define a coleção LV1
+         * Define os valores relacionado a chave atual.
+         */
+        this.config.set(key, new Collection());
+        for (const [key2, val2] of Object.entries(options[key])) {
+          if (typeof val2 === 'object') {
+            /**
+             * Define a coleção LV2
+             * Define os valores relacionado a chave atual.
+             */
+            this.config.get(key).set(key2, new Collection());
+            for (const [key3, val3] of Object.entries(options[key][key2])) {
+              if (typeof val3 === 'object') {
+                this.config.get(key).get(key2).set(key3, val3);
+              } else {
+                this.config.get(key).set(key2, val2);
+              }
+            }
+          } else {
+            this.config.get(key).set(key2, val2);
+          }
+        }
+      } else {
+        this.config.set(key, options[key]);
+      }
+    }
+  }
+
+  has(key: string): boolean {
+    return this.config.has(key);
+  }
+
+  set(key: string, val: any): boolean {
+    this.config.set(key, val);
+    return this.has(key);
+  }
+
+  get(key: string): any {
+    return this.config.get(key);
+  }
+}
