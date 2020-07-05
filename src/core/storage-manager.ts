4dd71b662af5e5b77341b3567256e0c7cd794fd8
Julio Cesar <jcmljunior@gmail.com>
2020-07-05 19:54:56 -0300

    adicionado config-manager em storage

--- src/core/storage-manager.ts
+++ src/core/storage-manager.ts
@@ -1,5 +1,15 @@
+import { Client } from 'discord.js';
+import ConfigManager from './config-manager';
+
 /**
  * Responsavel pela administração do Client Discord.
  * Client Eveby
  */
-export default class StorageManager {}
+export default class StorageManager extends Client {
+  config: ConfigManager;
+
+  constructor(config: ConfigManager, options: any) {
+    super(options);
+    this.config = config;
+  }
+}
