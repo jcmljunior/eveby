770a7e6c6fd51ff44f2d6ce51fb38e82e9dae9d0
Julio Cesar <jcmljunior@gmail.com>
2020-07-06 13:25:58 -0300

    definido documentação

--- src/@types/core/bootlets-manager.d.ts
+++ src/@types/core/bootlets-manager.d.ts
@@ -1,1 +1,23 @@
-export interface BootletsManagerConfig {}
+import StorageManager from '../../core/storage-manager';
+
+export interface BootletsManagerConfig {
+  /**
+   * Define o Client Eveby.
+   */
+  storage: StorageManager;
+
+  /**
+   * Define o local dos eventos.
+   */
+  path: string;
+
+  /**
+   * Define o local dos eventos.
+   */
+  setPath(): void;
+
+  /**
+   * Define a coleção de eventos.
+   */
+  setBootlet(): void;
+}
