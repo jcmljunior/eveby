a600b4d71b0f3d7aad9c31e9009e09a118a5a8d2
Julio Cesar <jcmljunior@gmail.com>
2020-07-05 21:13:23 -0300

    inserido metodo load/run

--- src/@types/core/eveby.d.ts
+++ src/@types/core/eveby.d.ts
@@ -4,4 +4,7 @@ import StorageManager from '../../core/storage-manager';
 export interface EvebyConfig {
   config: ConfigManager;
   storage: StorageManager;
+
+  load(): Promise<any>;
+  run(): Promise<any>;
 }
