8ddbf53788b21044bcf50e254243b4313b64a2f8
Julio Cesar <jcmljunior@gmail.com>
2020-07-06 01:17:24 -0300

    definido metodos config,bootlet e path

--- src/core/bootlets-manager.ts
+++ src/core/bootlets-manager.ts
@@ -1,4 +1,33 @@
+import StorageManager from './storage-manager';
+import { Collection } from 'discord.js';
+
 /**
  * Responsavel pelo carregamento dos eventos.
  */
-export default class BootletsManager {}
+export default class BootletsManager {
+  storage: StorageManager;
+  path!: string;
+
+  constructor(storage: StorageManager) {
+    this.storage = storage;
+
+    this.setPath();
+    this.setBootlet();
+  }
+
+  get config() {
+    return this.storage.get('config');
+  }
+
+  private setPath() {
+    this.path = this.config.get('path') + 'bootlets/';
+  }
+
+  private setBootlet() {
+    this.storage.set('bootlets', new Collection());
+  }
+
+  async load(): Promise<any> {}
+
+  async run(): Promise<any> {}
+}
