5ab0b26bba3f9c61e269dacc6c919e978b16c5ee
Julio Cesar <jcmljunior@gmail.com>
2020-07-06 01:10:57 -0300

    definido caminho base

--- src/core/eveby.ts
+++ src/core/eveby.ts
@@ -1,6 +1,8 @@
 import { EvebyConfig } from '../@types/index';
 import ConfigManager from './config-manager';
 import StorageManager from './storage-manager';
+import BootletsManager from './bootlets-manager';
+import AddonsManager from './addons-manager';
 
 /**
  * Classe responsavel pela administração do projeto.
@@ -9,15 +11,22 @@ export default class Eveby implements EvebyConfig {
   config: ConfigManager;
   storage: StorageManager;
 
-  bootlets: any;
-  addons: any;
+  bootlets: BootletsManager;
+  addons: AddonsManager;
 
   constructor() {
     this.config = new ConfigManager();
+    this.config.register({ path: process.cwd() + '/dist/spices/' });
+
     this.storage = new StorageManager(this.config, {});
+
+    this.bootlets = new BootletsManager(this.storage);
+    this.addons = new AddonsManager();
   }
 
   async load(): Promise<any> {}
 
-  async run(): Promise<any> {}
+  async run(): Promise<any> {
+    console.log(this.bootlets);
+  }
 }
