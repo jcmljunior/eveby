557396b8190349affd24f84c6c1ffe3ae1153c02
Julio Cesar <jcmljunior@gmail.com>
2020-07-06 14:52:08 -0300

    definido metodo instance/caps

--- src/core/assistant-manager.ts
+++ src/core/assistant-manager.ts
@@ -1,5 +1,25 @@
+import StorageManager from './storage-manager';
+
 /**
  * Responsavel por partilhar os objetos comuns entre
  * as classes principais de eventos e comandos.
  */
-export default class AssistantManager {}
+export default class AssistantManager {
+  /**
+   * Verifica a propriedade do objeto informado e
+   * o instancia.
+   * @param obj Objeto a ser instanceado.
+   * @param storage Predefinições do Storage.
+   */
+  instance(obj: any, storage: StorageManager): any {
+    return new obj[Object.keys(obj)[0]](storage);
+  }
+
+  /**
+   * Transforma a primeira letra em maiuscula. (Capitalize)
+   * @param context
+   */
+  caps(context: string): any {
+    return context.charAt(0).toUpperCase() + context.slice(1);
+  }
+}
